781214ded3636a94846a99f1b947db35fc139a68
Yanlong-li <ahlyl94@gmail.com>
2019-08-13 17:26:03 +0800

    移植ThinkPHP验证器 根据ThinkPHP的Request调整部分方法逻辑

--- src/Request.php
+++ src/Request.php
@@ -27,38 +27,82 @@ class Request
     public static function param($name = null, $default = null)
     {
         // 合并Get和Post数据，以Post为主覆盖Get数据
-        $params = array_merge(self::get(), self::post());
-        if (is_string($name) && strlen(trim($name)) > 0) {
+        $params = array_merge(static::get(), static::post());
 
-            $suffix = substr($name, -2);
-            if (in_array($suffix, [
-                '/s',//字符型
-                '/d',//整型
-                '/b',//布尔型
-                '/a',//数组
-                '/f',//浮点型
-            ])) {
-                $name = substr($name, 0, -2);
-            } else {
-                $suffix = null;
-            }
+        return static::input($params, $name, $default);
+    }
 
-            if (isset($params[$name])) {
-                return self::modification($params[$name], $suffix);
-            }
+    /**
+     * 获取指定的参数
+     * @access public
+     * @param string|array $name 变量名
+     * @param string $type 变量类型
+     * @return mixed
+     */
+    public static function only($name, $type = 'param')
+    {
 
-            return $default;
+        $param = static::$type();
 
+        if (is_string($name)) {
+            $name = explode(',', $name);
         }
 
-        return $params;
+        $item = [];
+        foreach ($name as $key => $val) {
+
+            if (is_int($key)) {
+                $default = null;
+                $key = $val;
+            } else {
+                $default = $val;
+            }
+
+            if (isset($param[$key])) {
+                $item[$key] = $param[$key];
+            } elseif (isset($default)) {
+                $item[$key] = $default;
+            }
+        }
+
+        return $item;
     }
+
+    /**
+     * 排除指定参数获取
+     * @access public
+     * @param string|array $name 变量名
+     * @param string $type 变量类型
+     * @return mixed
+     */
+    public static function except($name, $type = 'param')
+    {
+        $param = static::$type();
+        if (is_string($name)) {
+            $name = explode(',', $name);
+        }
+
+        foreach ($name as $key) {
+            if (isset($param[$key])) {
+                unset($param[$key]);
+            }
+        }
+
+        return $param;
+    }
+
 
     public static function get($name = null, $default = null)
     {
-        return \Yii::$app->request->get($name, $default);
+        $data = \Yii::$app->request->get($name, $default);
+        return static::input($data, $name, $default);
     }
 
+    /**
+     * @param null $name
+     * @param null $default
+     * @return mixed
+     */
     public static function post($name = null, $default = null)
     {
         $rawContentType = \Yii::$app->request->getContentType();
@@ -88,25 +132,199 @@ class Request
 
         }
 
-        return $data;
+        return static::input($data, $name, $default);
     }
 
+    /**
+     * 是否为GET请求
+     * @access public
+     * @return bool
+     */
     public static function isGet()
     {
         return \Yii::$app->request->isGet;
     }
 
-    public static function modification($value, $type)
+    /**
+     * 是否为POST请求
+     * @access public
+     * @return bool
+     */
+    public static function isPost()
+    {
+        return \Yii::$app->request->isPost;
+    }
+
+    /**
+     * 是否为PUT请求
+     * @access public
+     * @return bool
+     */
+    public static function isPut()
+    {
+        return \Yii::$app->request->isPut;
+    }
+
+    /**
+     * 是否为DELTE请求
+     * @access public
+     * @return bool
+     */
+    public static function isDelete()
+    {
+        return \Yii::$app->request->isDelete;
+    }
+
+    /**
+     * 是否为HEAD请求
+     * @access public
+     * @return bool
+     */
+    public static function isHead()
+    {
+        return \Yii::$app->request->isHead;
+    }
+
+    /**
+     * 是否为PATCH请求
+     * @access public
+     * @return bool
+     */
+    public static function isPatch()
+    {
+        return \Yii::$app->request->isPatch;
+    }
+
+    /**
+     * 是否为OPTIONS请求
+     * @access public
+     * @return bool
+     */
+    public static function isOptions()
+    {
+        return \Yii::$app->request->isOptions;
+    }
+
+    /**
+     * 是否为cli
+     * @access public
+     * @return bool
+     */
+    public static function isCli()
+    {
+        return PHP_SAPI == 'cli';
+    }
+
+    /**
+     * 当前是否ssl
+     * @access public
+     * @return bool
+     */
+    public static function isSsl()
+    {
+        return \Yii::$app->request->isSecureConnection;
+    }
+
+    /**
+     * 当前是否Ajax请求
+     * @access public
+     * @param bool $ajax true 获取原始ajax请求
+     * @return bool
+     */
+    public static function isAjax($ajax = false)
+    {
+        return \Yii::$app->request->isAjax;
+    }
+
+    /**
+     * 当前是否Pjax请求
+     * @access public
+     * @return bool
+     */
+    public static function isPjax()
+    {
+        return \Yii::$app->request->isPjax;
+    }
+
+    public static function method()
+    {
+        return \Yii::$app->request->method;
+    }
+
+    /**
+     * @param array $data
+     * @param string $name
+     * @param mixed $default
+     * @return mixed
+     */
+    protected static function input($data, $name, $default)
+    {
+        $name = (string)$name;
+        if ('' != $name) {
+            // 解析name
+            if (strpos($name, '/')) {
+                list($name, $suffix) = explode('/', $name);
+            }
+
+            $data = static::getData($data, $name);
+
+            if (is_null($data)) {
+                return $default;
+            }
+
+            if (is_object($data)) {
+                return $data;
+            }
+        }
+        // 强制类型转换
+        if (isset($suffix)) {
+            return static::typeCast($data, $suffix);
+        }
+        return $data;
+    }
+
+    /**
+     * 获取数据
+     * @access public
+     * @param array $data 数据源
+     * @param string|false $name 字段名
+     * @return mixed
+     */
+    protected static function getData(array $data, $name)
+    {
+        foreach (explode('.', $name) as $val) {
+            if (isset($data[$val])) {
+                $data = $data[$val];
+            } else {
+                return;
+            }
+        }
+
+        return $data;
+    }
+
+    /**
+     * 强制类型转换
+     * @access public
+     * @param string $value
+     * @param string $type
+     * @return mixed
+     */
+    public static function typeCast($value, $type)
     {
         $_value = null;
         switch ($type) {
-            case '/s':
-                $_value = (string)$value;
+            case 's':
+                if (is_scalar($value)) {
+                    $_value = (string)$value;
+                } else {
+                    throw new \InvalidArgumentException('variable type error：' . gettype($value));
+                }
                 break;
-            case '/d':
+            case 'd':
                 $_value = (integer)$value;
                 break;
-            case '/b':
+            case 'b':
                 if (in_array(strtolower($value), ['true', 1, 't'])) {
                     $value = true;
                 }
@@ -115,10 +333,10 @@ class Request
                 }
                 $_value = (boolean)$value;
                 break;
-            case '/a':
+            case 'a':
                 $_value = (array)$value;
                 break;
-            case '/f':
+            case 'f':
                 $_value = (float)$value;
                 break;
             default:
